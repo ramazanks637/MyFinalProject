a02d4b74e930e8a32a8a993e1258e8331a6f79bf
Ramazan <181312064@stu.kmu.edu.tr>
2021-02-05 22:21:32 +0300

    son

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -9,7 +9,7 @@ namespace DataAccess.Abstract
     public interface IProductDal
     {
         List<Product> GetAll();
-        void Add(Product product);
+        void Add(Product product); 
         void Update(Product product);
         void Delete(Product product);
 
